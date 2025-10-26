4e7eab706e00f64bebe5e82267b6cd1da1d97b1a
Ficelo <teiva.trillard@gmail.com>
2025-10-26 16:48:22 +0200

    Add pages and routing

--- metrodle/src/App.jsx
+++ metrodle/src/App.jsx
@@ -1,37 +1,83 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
+import { MetroPage } from "./pages/metro/MetroPage.jsx"
+
+function NavBar() {
+
+  return (
+    <div className='top-bar'>
+      <div className='menu'>
+        <div className='metro-station'>
+          <Link to="/combo" className='metro-link'>COMBO</Link>
+          <div className='metro-circle'></div>
+        </div>
+        <div className='metro-station'>
+          <Link to="/tram" className='metro-link'>TRAM</Link>
+          <div className='metro-circle'></div>
+        </div>
+        <div className='metro-station'>
+          <Link to="/rer" className='metro-link'>RER</Link>
+          <div className='metro-circle'></div>
+        </div>
+        <div className='metro-station'>
+          <Link to="/" className='metro-link'>METRO</Link>
+          <div className='metro-circle'></div>
+        </div>
+        <div className='metro-station'>
+          <Link to="/infos" className='metro-link'>INFOS</Link>
+          <div className='metro-circle'></div>
+        </div>
+      </div>
+      <div className='metro-bar'></div>
+    </div>
+  );
+
+}
+
+function RerPage() {
+  return (
+    <div></div>
+  );
+}
+
+function TramPage() {
+  return (
+    <div></div>
+  );
+}
+
+function ComboPage() {
+  return (
+    <div></div>
+  );
+}
+
+
+function InfoPage() {
+  return (
+    <div></div>
+  );
+}
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-        <div className='top-bar'>
-          <div className='menu'>
-            <div className='metro-station'>
-              <p>COMBO</p>
-              <div className='metro-circle'></div>
-            </div>
-            <div className='metro-station'>
-              <p>TRAM</p>
-              <div className='metro-circle'></div>
-            </div>
-            <div className='metro-station'>
-              <p>RER</p>
-              <div className='metro-circle'></div>
-            </div>
-            <div className='metro-station'>
-              <p className='metro-selected' >METRO</p>
-              <div className='metro-circle'></div>
-            </div>
-            <div className='metro-station'>
-              <p>INFO</p>
-              <div className='metro-circle'></div>
-            </div>
-          </div>
-          <div className='metro-bar'></div>
-        </div>
+      <BrowserRouter>
+
+        <NavBar></NavBar>
+
+        <Routes>
+          <Route path="/" element={<MetroPage></MetroPage>}></Route>
+          <Route path="/combo" element={<RerPage></RerPage>}></Route>
+          <Route path="/rer" element={<TramPage></TramPage>}></Route>
+          <Route path="/tram" element={<ComboPage></ComboPage>}></Route>
+          <Route path="/infos" element={<InfoPage></InfoPage>}></Route>
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
---
+++ metrodle/src/pages/metro/MetroPage.jsx
@@ -0,0 +1,10 @@
+import "./MetroPage.css";
+import "../../App.css";
+
+export function MetroPage() {
+  return (
+    <div >
+      <h1 className='page-title'>METRODLE</h1>
+    </div>
+  );
+}
